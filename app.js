1c6d02b82bc275e77215dfee31b435e235c7f096
mblason <marina.blasongraviz@gmail.com>
2022-07-15 18:37:24 +0200

    CSS done

--- app.js
+++ app.js
@@ -1,22 +1,20 @@
-const express = require('express');
-const app = express();
+const express = require('express')
+const app = express()
 
-app.use(express.static('public'))
+app.use(express.static(__dirname + '/public'))
+
+app.set('views', path.join(__dirname, 'views'));
 
 app.get('/', (req, res, next) => { 
-    res.sendFile(__dirname + '/views/home.html')
-})
-
-app.get('/works', (req, res, next) => { 
-    res.sendFile(__dirname + '/views/works.html')
+    res.sendFile(__dirname + 'home.html')
 })
 
 app.get('/about', (req, res, next) => { 
-    res.sendFile(__dirname + '/views/about.html')
+    res.sendFile(__dirname + 'about.html')
 })
 
-app.get('/notfound', (req, res, next) => { 
-    res.sendFile(__dirname + '/views/notFound.html')
+app.get('/works', (req, res, next) => { 
+    res.sendFile(__dirname + 'works.html')
 })
 
 app.listen(3000, () => {
